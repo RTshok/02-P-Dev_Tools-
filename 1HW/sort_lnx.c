5bfb193c2c65b2192d3df54ed935e1aac68be0c4
Oleg Kuzhylny <Jayawardenepura>
2018-07-15 19:41:00 +0300

    new commit for readabitily

--- 1HW/sort_lnx.c
+++ 1HW/sort_lnx.c
@@ -20,10 +20,6 @@ double timespec_diff(struct timespec *stop, struct timespec *start)
 	diff += (stop->tv_nsec - start->tv_nsec) / 1e6;
 	return diff;
 }
-
-
-
-
 int main(int argc, char *argv[])
 {
 	int *items;
@@ -33,58 +29,55 @@ int main(int argc, char *argv[])
 /* Parse arguments */
 if (argc == 1) goto exc_noargs;
 if (argc > 1) 
+
 strcpy(FileName,argv[1]);
 if(argc == 3){
- amount=atoi(argv[2]);
-items= malloc(amount*sizeof(*items));
+ amount = atoi(argv[2]);
+items = malloc(amount*sizeof(*items));
 if(items == NULL)
 goto exc_noargs;
 }
 	
-	if (argc > 4)
-		goto exc_argtoomuch;
+if (argc > 4) goto exc_argtoomuch;
 
-	
+FILE *file,*fp_rand;
+file = fopen (FileName,"w");
+fprintf(file,"#AmountOfElements,N		#TimeOfSorting,ms");
 
-
-	FILE *file,*fp_rand;
-	file = fopen (FileName,"w");
-	fprintf(file,"#AmountOfElements,N		#TimeOfSorting,ms");
-	
-	/* Initialize pseudo-random generator with truly random number */
-			if ((fp_rand = fopen("/dev/random", "rb")) == NULL) goto exc_fopen;
-			unsigned int seed;
-			fread(&seed, sizeof(seed), 1, fp_rand);
-			if (ferror(fp_rand)) goto exc_fread;
-			fclose(fp_rand);
-			srand(seed);
-		int *array;
-	 	double time=0;
+/* Initialize pseudo-random generator with truly random number */
+		if ((fp_rand = fopen("/dev/random", "rb")) == NULL) goto exc_fopen;
+		unsigned int seed;
+		fread(&seed, sizeof(seed), 1, fp_rand);
+		if (ferror(fp_rand)) goto exc_fread;
+		fclose(fp_rand);
+		srand(seed);
+	int *array;
+ 	double time=0;
 		
-for (int i=0; i<amount;i++)
-		{
+for (int i=0; i < amount;i++)
+{
 			
-			
-			items[i] = (i*i*i*amount+amount)*3;//generating the size of future array;
-			/* Fill array with random numbers */
-			array = malloc(items[i]*sizeof(*array));
-			if (array == NULL) goto exc_malloc;
-			unsigned int cnt;
-			for (cnt = 0; cnt < items[i]; cnt++)
-				array[cnt] = rand();
-			
-			/* Sort array */
-			struct timespec time_now, time_after;
-			clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &time_now);
-			insertion_sort(array,items[i]);
-			clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &time_after);
-    			time = timespec_diff(&time_after, &time_now);
-			fprintf(file,"\n%d					%g",items[i],time);
-			printf("Time,ms :%g, Amount :%d",time,items[i]);
-	
-			free(array);
-	
-		}
+	items[i] = (i*i*i*amount+amount)*3;//generating the size of future array;
+	/* Fill array with random numbers */
+	array = malloc(items[i]*sizeof(*array));
+	if (array == NULL) goto exc_malloc;
+	unsigned int cnt;
+	for (cnt = 0; cnt < items[i]; cnt++)
+		array[cnt] = rand();
+
+	/* Sort array */
+	struct timespec time_now, time_after;
+	clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &time_now);
+	insertion_sort(array,items[i]);
+	clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &time_after);
+
+	time = timespec_diff(&time_after, &time_now);
+	fprintf(file,"\n%d					%g",items[i],time);
+	printf("Time,ms :%g, Amount :%d",time,items[i]);
+
+	free(array);
+
+}
 
 free(items);
 fclose(file);
